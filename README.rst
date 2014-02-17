c26241ef871d838e5aaea1341cdb5b83b93a2bde
Chris Lamb <chris@chris-lamb.co.uk>
2014-02-17 20:35:04 +0000

    Initial commit.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

---
+++ README.rst
@@ -0,0 +1,21 @@
+Strava: Strava: Running TSS estimator
+"""""""""""""""""""""""""""""""""""""
+
+A Chrome extension to estimate a Training Stress Score (TSS) from a run's
+(grade-adjusted) pace distribution.
+
+"Suffer Score" is only available when using a heart rate monitor.
+
+Installation
+============
+
+ * Clone (or otherwise download) the contents of this repository.
+
+ * In Chrome, go to "Tools > Extensions".
+
+ * Ensure "Developer mode" is ticked.
+
+ * Click "Load unpacked extension" and select the directory you have stored
+   this code.
+
+ * Refresh any page on Strava - the extension will now be active.
---
+++ js/injected.js
@@ -0,0 +1,33 @@
+var TSS_PER_HOUR = {
+  'Z1':  60,
+  'Z2': 100,
+  'Z3': 120,
+  'Z4': 160,
+  'Z5': 300,
+  'Z6': 600
+};
+
+var view = Strava.Labs.Activities.PaceZones;
+var fn = view.prototype.getPaceZones;
+
+view.prototype.getPaceZones = function () {
+  var result = fn.apply(this, Array.prototype.slice.call(arguments));
+
+  var tss = 0;
+  jQuery.each(this.paceZones, function (i, item) {
+    // Re-parse the time (eg. "23s" / "32:21")
+    var parts = item.time.replace('s', '').split(':').reverse();
+    var seconds = parts.reduce(function (prev, cur, idx) {
+        return prev + (cur * Math.pow(60, idx));
+    }, 0);
+
+    // Calculate the contribution to TSS
+    tss += TSS_PER_HOUR[item.name] / 3600 * seconds;
+  });
+
+  jQuery('#view .inline-stats').append(
+    '<li><strong>' + Math.round(tss) + '</strong><div class="label">TSS (estimated)</div></li>'
+  );
+
+  return result;
+};
